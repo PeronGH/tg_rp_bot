4793aed516abcc7365c1dbc98ad43ae1549d17d7
PeronGH <peron_ap@icloud.com>
2025-02-13 17:05:24 +0000

    feat: utility to set webhooks

---
+++ set_webhook.ts
@@ -0,0 +1,14 @@
+import { Bot } from "grammy";
+
+const botToken = Deno.env.get("TG_BOT_TOKEN") ?? prompt("Bot Token:");
+if (!botToken) {
+  throw new Error("Bot token is not provided");
+}
+
+const bot = new Bot(botToken);
+
+const endpoint = Deno.args[0] ?? prompt("Webhook Endpoint:");
+
+await bot.api.setWebhook(endpoint);
+
+console.log("Webhook Endpoint is successfully set");
