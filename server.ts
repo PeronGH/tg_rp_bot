516a2698979b62181c241ff3dfec5b1f47f3ba08
PeronGH <peron_ap@icloud.com>
2025-02-13 22:32:53 +0000

    feat: global error handling for server

--- server.ts
+++ server.ts
@@ -2,6 +2,17 @@ import { webhookCallback } from "grammy";
 import { bot } from "./bot.ts";
 import { TG_BOT_SECRET_TOKEN } from "./env.ts";
 
+const handler = webhookCallback(bot, "std/http", {
+  secretToken: TG_BOT_SECRET_TOKEN,
+});
+
 Deno.serve(
-  webhookCallback(bot, "std/http", { secretToken: TG_BOT_SECRET_TOKEN }),
+  async (req) => {
+    try {
+      return await handler(req);
+    } catch (error) {
+      console.error("CaughtError:", error);
+      return new Response("500 Internal Error", { status: 500 });
+    }
+  },
 );
