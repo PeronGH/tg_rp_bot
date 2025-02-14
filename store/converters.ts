741f4882f9cb59afb1f5da529c344303ce93775a
PeronGH <peron_ap@icloud.com>
2025-02-14 00:32:07 +0000

    feat: better handling of captions

--- store/converters.ts
+++ store/converters.ts
@@ -2,22 +2,33 @@ import type { Message } from "grammy/types";
 import { StoreMessageParams, storeMessageParamsSchema } from "./schema.ts";
 
 export function toStoredMessageSafe(
-  { chat, message_id, from, reply_to_message, text }: Message,
+  { message_id, from, reply_to_message, text, caption, chat }: Message,
 ) {
-  return storeMessageParamsSchema.safeParse({
+  const storeMsg = {
     chatId: chat.id,
     messageId: message_id,
-    fromName: from?.last_name
-      ? `${from.first_name} ${from.last_name}`
-      : from?.first_name,
+    fromName: formatName(from?.first_name, from?.last_name) ?? "Unknown",
     fromId: from?.id,
-    text,
+    text: text ?? caption,
     replyToMessageId: reply_to_message?.message_id, // TODO: fix potential bug with forwarded message (unsure if the bug exists)
-  });
+  };
+  return storeMessageParamsSchema.safeParse(storeMsg);
 }
 
 export function toStoredMessage(msg: Message): StoreMessageParams {
   const result = toStoredMessageSafe(msg);
   if (!result.success) throw result.error;
   return result.data;
+}
+
+function formatName(
+  firstName: string | undefined,
+  lastName: string | undefined,
+): string | undefined {
+  // Both present
+  if (firstName && lastName) return `${firstName} ${lastName}`;
+  // Only first name
+  if (firstName) return firstName;
+  // Only last name
+  if (lastName) return lastName;
 }
